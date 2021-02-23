f9f1394c0459b9b33d483c68bb4817cbc727f872
Kamil <qkiz288@gmail.com>
2021-02-23 08:59:03 +0100

    Allocation message flow

---
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationListener.java
@@ -0,0 +1,35 @@
+package guru.sfg.beer.inventory.service.services;
+
+import com.kkukielka.brewery.model.events.AllocateOrderRequest;
+import com.kkukielka.brewery.model.events.AllocateOrderResult;
+import guru.sfg.beer.inventory.service.config.JmsConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AllocationListener {
+    private final AllocationService allocationService;
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
+    public void listen(AllocateOrderRequest request) {
+        AllocateOrderResult.AllocateOrderResultBuilder builder = AllocateOrderResult.builder();
+        builder.beerOrderDto(request.getBeerOrderDto());
+
+        try {
+            Boolean allocationResult = allocationService.allocateOrder(request.getBeerOrderDto());
+
+            builder.pendingInventory(!allocationResult);
+        } catch (Exception e) {
+            log.error(String.format("Allocation failed for order ID: %s", request.getBeerOrderDto().getId()));
+            builder.allocationError(true);
+        }
+
+        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE, builder.build());
+    }
+}
